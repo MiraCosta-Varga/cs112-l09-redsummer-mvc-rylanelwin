b1ce4293c19480ce8b23b8cda0fcaa99104ed10f
RylanElwin <rylanelwin@gmail.com>
2024-11-16 16:23:48 -0800

    completed
    i typed one image path as "/image" instead of "/images" and spent 30 minutes trying to figure out why it wasnt working

--- src/main/java/cs112/lab09/MainApplication.java
+++ src/main/java/cs112/lab09/MainApplication.java
@@ -10,8 +10,9 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 400, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/cs112/lab09/controllers/MainController.java
+++ src/main/java/cs112/lab09/controllers/MainController.java
@@ -1,14 +1,38 @@
 package cs112.lab09.controllers;
 
+import cs112.lab09.MainApplication;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
-public class MainController {
-    @FXML
-    private Label welcomeText;
+import java.io.IOException;
 
+
+public class MainController{
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button startButton;
+    @FXML
+    private Button quitButton;
+    @FXML
+    protected void onStartButtonClicked(ActionEvent actionEvent) throws IOException {
+
+        Parent mapView = FXMLLoader.load(MainApplication.class.getResource("map-view.fxml"));
+        Scene mapViewScene = new Scene(mapView);
+
+        Stage window = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+
+        window.setScene(mapViewScene);
+
+        window.show();
+    }
+    @FXML
+    protected void onQuitButtonClicked(ActionEvent actionEvent){
+        Platform.exit();
     }
 }
